e419e87f02888ccb6156ca6f408e52f3d4af7e72
Eliran Assaraf <eliran.a.bg@gmail.com>
2018-08-19 19:56:06 -0400

    first test

---
+++ services.js
@@ -0,0 +1,79 @@
+//Admin functions
+routerApp.factory('Admin', function () {
+
+    var factory = {};
+    factory.isloggedin = false;
+
+    factory.isAdmin = function() {
+        if(factory.isloggedin)
+            return 'Admin mode';
+        else
+            return 'User mode';
+
+    }
+
+    factory.setloggedin = function(loggedin) {
+        factory.isloggedin = loggedin;
+    }
+
+    return factory;
+})
+
+//functions controller name list
+routerApp.factory('List', function($http) {
+    
+    var factory = {};
+
+    factory.getList = function() 
+    {
+        return $http({
+            method: 'GET',
+            url: 'adminpage/downloadList.php'
+        });
+    }
+
+    factory.postList = function(namelist)
+    {
+        return $http({
+            method: 'POST',
+            url: 'homepage/uploadList.php',
+            data: 'list=' + namelist,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        });
+    }
+
+    factory.removeName = function (index)
+    {
+        return $http({
+            method: 'POST',
+            url: 'uploadList.php',
+            data: 'index=' + index,
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+        });
+
+    }
+    /*
+    factory.displayList = function (list)
+    {
+        var str = "oooo";
+
+        for(var i = 0; i < list.length; i++)
+        {
+            str+="yoo";
+            str += list[i].name + ",";
+
+            if(list[i].ischild === true)
+                str += list[i].ischild;
+            else
+                str += 0 ;
+
+            str += "<br>";
+        }
+
+        return str;
+    }*/
+
+
+
+    return factory;
+})
---
+++ uploadList.php
@@ -0,0 +1,32 @@
+<?php
+/* DEVELOPER TESTS */
+var_dump($_POST["list"]);
+
+//OPEN FILE ang get 'list'
+$myfile = fopen("textfile.txt", "r+") or die ("Just die");
+
+//get original list and updated list
+$oglist = fread($myfile, filesize("testfile.txt"));
+$updatedList = $_POST["list"];
+
+//decode lists, if empty create it to avoid errors
+if (empty($oglist))
+{
+    $jsonOglist = array();
+}
+else
+    $jsonOglist = json_decode($oglist);
+
+$jsonlist = json_decode($updatedList);
+
+//merge updated list to old list
+$jsonOglist = array_merge($jsonOglist, $jsonlist);
+
+//encode the list back
+$oglist = json_encode($jsonOglist);
+
+//WRITE TO TEXT
+file_put_contents("testfile.txt", "$oglist", FILE_USE_INCLUDE_PATH);
+fclose($myfile);
+?>
+
